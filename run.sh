f45a2db6cb90d986df4b60d48ee8be7925f868bb
DKZ <davidkingzyb@163.com>
2020-02-19 14:13:38 +0000

    analysis.py

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 DATE=$(date +%m%d_%H%M)
 ps aux | grep pccoldmain | awk '{print $2}'|xargs kill -9 
 ps aux | grep pccolddanmu | awk '{print $2}'|xargs kill -9 
-nohup python3 -u /home/pi/.local/bin/pccold -n pccoldmain >logfile/pccold_${DATE}.log 2>&1 &
-nohup python3 -u /home/pi/.local/bin/pccolddanmu -n pccolddanmu >logfile/danmu_${DATE}.log 2>&1 &
+nohup python3 -u /usr/local/bin/pccold -n pccoldmain >logfile/pccold_${DATE}.log 2>&1 &
+nohup python3 -u /usr/local/bin/pccolddanmu -n pccolddanmu >logfile/danmu_${DATE}.log 2>&1 &
